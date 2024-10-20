0c73ff91b28f755283866ba3ca291d9466e20b0e
EnmanuelDevs <enmanueldevs@gmail.com>
2024-10-19 22:36:13 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraysIntElements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArraySum_Click(object sender, EventArgs e)
+        {
+            string[] numbersStr = txtNumbers.Text.Split(',');
+
+            int[] numbers = Array.ConvertAll(numbersStr, int.Parse);
+
+            int sum = 0; 
+
+            foreach (int number in numbers)
+            sum += number;
+
+            lblResult.Text = "Sum is: " + sum;
+        }
+    }
+}
